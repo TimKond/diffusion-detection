411eacf261698abd3006576a5e9da4673cf6c690
TimKond <timkondziella2@gmail.com>
2023-06-02 17:22:56 +0200

    data split and co

---
+++ data/negatives-fetch.py
@@ -0,0 +1,8 @@
+import pathlib
+script_location = pathlib.Path(__file__).parent.resolve()
+
+from datasets import load_dataset
+
+# use !huggingface-cli login
+# If the dataset is gated/private, make sure you have run huggingface-cli login
+dataset = load_dataset("imagenet-1k")
